a1748dd7d1dbd90dde6ee6b6471f75d6d4deb1a0
SdrgonLee <doe06100@gmail.com>
2025-07-02 17:31:08 +0900

    gallery masonry

--- frontend/src/pages/Gallery/Gallery.jsx
+++ frontend/src/pages/Gallery/Gallery.jsx
@@ -6,34 +6,64 @@ const Gallery = () => {
     <div className='gallery-page'>
       <div className='gallery-container'>
         <div className='gallery-item'>
-          <img src='../../assets/images/portfolio-1.jpg' alt='portfolio-1' className='gallery-image' />
+          <img src='../../src/assets/gallery/random1000/image_0001_1024x1200.png' alt='portfolio-1' className='gallery-image' />
         </div>
         <div className='gallery-item'>
-          <img src='../../assets/images/portfolio-2.jpg' alt='portfolio-2' className='gallery-image' />
+        <img src='../../src/assets/gallery/random1000/image_0002_1024x1200.png' alt='portfolio-2' className='gallery-image' />
         </div>
         <div className='gallery-item'>
-          <img src='../../assets/images/portfolio-2.jpg' alt='portfolio-2' className='gallery-image' />
+        <img src='../../src/assets/gallery/random1000/image_0003_1440x800.png' alt='portfolio-3' className='gallery-image' />
         </div>
         <div className='gallery-item'>
-          <img src='../../assets/images/portfolio-2.jpg' alt='portfolio-2' className='gallery-image' />
+        <img src='../../src/assets/gallery/random1000/image_0004_1440x1200.png' alt='portfolio-4' className='gallery-image' />
         </div>
         <div className='gallery-item'>
-          <img src='../../assets/images/portfolio-2.jpg' alt='portfolio-2' className='gallery-image' />
+        <img src='../../src/assets/gallery/random1000/image_0005_1024x800.png' alt='portfolio-5' className='gallery-image' />
         </div>
         <div className='gallery-item'>
-          <img src='../../assets/images/portfolio-2.jpg' alt='portfolio-2' className='gallery-image' />
+        <img src='../../src/assets/gallery/random1000/image_0006_1440x800.png' alt='portfolio-6' className='gallery-image' />
         </div>
         <div className='gallery-item'>
-          <img src='../../assets/images/portfolio-2.jpg' alt='portfolio-2' className='gallery-image' />
+        <img src='../../src/assets/gallery/random1000/image_0007_1024x800.png' alt='portfolio-7' className='gallery-image' />
         </div>
         <div className='gallery-item'>
-          <img src='../../assets/images/portfolio-2.jpg' alt='portfolio-2' className='gallery-image' />
+        <img src='../../src/assets/gallery/random1000/image_0008_1440x1200.png' alt='portfolio-8' className='gallery-image' />
         </div>
         <div className='gallery-item'>
-          <img src='../../assets/images/portfolio-2.jpg' alt='portfolio-2' className='gallery-image' />
+        <img src='../../src/assets/gallery/random1000/image_0009_2550x800.png' alt='portfolio-9' className='gallery-image' />
         </div>
         <div className='gallery-item'>
-          <img src='../../assets/images/portfolio-2.jpg' alt='portfolio-2' className='gallery-image' />
+        <img src='../../src/assets/gallery/random1000/image_0010_2550x1200.png' alt='portfolio-10' className='gallery-image' />
+        </div>
+        <div className='gallery-item'>
+          <img src='../../src/assets/gallery/random1000/image_0001_1024x1200.png' alt='portfolio-1' className='gallery-image' />
+        </div>
+        <div className='gallery-item'>
+        <img src='../../src/assets/gallery/random1000/image_0002_1024x1200.png' alt='portfolio-2' className='gallery-image' />
+        </div>
+        <div className='gallery-item'>
+        <img src='../../src/assets/gallery/random1000/image_0003_1440x800.png' alt='portfolio-3' className='gallery-image' />
+        </div>
+        <div className='gallery-item'>
+        <img src='../../src/assets/gallery/random1000/image_0004_1440x1200.png' alt='portfolio-4' className='gallery-image' />
+        </div>
+        <div className='gallery-item'>
+        <img src='../../src/assets/gallery/random1000/image_0005_1024x800.png' alt='portfolio-5' className='gallery-image' />
+        </div>
+        <div className='gallery-item'>
+        <img src='../../src/assets/gallery/random1000/image_0006_1440x800.png' alt='portfolio-6' className='gallery-image' />
+        </div>
+        <div className='gallery-item'>
+        <img src='../../src/assets/gallery/random1000/image_0007_1024x800.png' alt='portfolio-7' className='gallery-image' />
+        </div>
+        <div className='gallery-item'>
+        <img src='../../src/assets/gallery/random1000/image_0008_1440x1200.png' alt='portfolio-8' className='gallery-image' />
+        </div>
+        <div className='gallery-item'>
+        <img src='../../src/assets/gallery/random1000/image_0009_2550x800.png' alt='portfolio-9' className='gallery-image' />
+        </div>
+        <div className='gallery-item'>
+        <img src='../../src/assets/gallery/random1000/image_0010_2550x1200.png' alt='portfolio-10' className='gallery-image' />
         </div>
       </div>
     </div>
